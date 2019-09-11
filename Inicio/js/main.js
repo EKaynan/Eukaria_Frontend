f695cb91a35d7a3e494251ed86a3210f092343b6
EKaynan <erickaynan11@gmail.com>
2019-09-11 13:53:10 -0400

    CA melhorada

--- Inicio/js/main.js
+++ Inicio/js/main.js
@@ -1,101 +1,43 @@
+const seta_1=document.querySelector('.s1');
+const seta_2=document.querySelector('.s2');
+const seta_3=document.querySelector('.s3');
+
+anime({
+	targets: '.s1',
+	opacity: 0.4,
+	direction: 'alternate',
+	duration: 500,
+	delay: 200,
+	endDelay: 300,
+	loop: true
+});
+
+anime({
+	targets: '.s2',
+	opacity: 0.7,
+	direction: 'alternate',
+	duration: 500,
+	delay: 250,
+	endDelay: 250,
+	loop: true
+});
+
+anime({
+	targets: '.s3',
+	opacity: 1,
+	direction: 'alternate',
+	duration: 500,
+	delay: 300,
+	endDelay: 200,
+	loop: true
+});
+
+$(".scroll").click(function(){
+
+$('html, body').animate({
+        scrollTop: $(".apresentacao").offset().top
+    }, 500);
+});
 
 
-var aberto=false;
 
-function abrir() { 
-	anime({
-		targets: '.symbol',
-		duration: 100,
-		easing: 'linear',
-		rotate: '180deg'
-	});
-
-	anime({
-		targets: '.top',
-		duration: 100,
-		easing: 'linear',
-		opacity: 0
-	})
-	anime({
-		targets: '.bottom',
-		easing: 'linear',
-		duration: 100,
-		opacity: 0
-	})
-
-	anime({
-		targets: '.mid',
-		easing: 'linear',
-		width: '15px',
-		height: '15px',
-		borderRadius: '50%',
-		// backgroundColor: '#27ae60',
-		duration: 100
-	});
-	aberto=true;
-};
-function fechar() { 
-	anime({
-		targets: '.symbol',
-		duration: 100,
-		easing: 'linear',
-		rotate: '0deg'
-	});
-	anime({
-		targets: '.top',
-		duration: 100,
-		easing: 'linear',
-		opacity: 1
-	})
-	anime({
-		targets: '.bottom',
-		easing: 'linear',
-		duration: 100,
-		opacity: 1
-	})
-
-	anime({
-		targets: '.mid',
-		easing: 'linear',
-		width: '30px',
-		height: '4px',
-		borderRadius: '0%',
-		// backgroundCoFlor: '#000000',
-		duration: 100
-	});
-	aberto=false;
-};
-function expandir(){
-	anime({
-		targets:'.menu',
-		easing: 'linear',
-		width: '300px',
-		height: '400px',
-		borderRadius: '5%',
-		duration:100
-	});
-	document.querySelector('.lista').style.color= '#FFFFFF';
-}
-function diminuir(){
-	anime({
-		targets:'.menu',
-		easing: 'linear',
-		height: '60px',
-		width: '60px',
-		borderRadius: '50%',
-		duration:100
-	});
-	document.querySelector('.lista').style.color= '#000000';
-}
-
-function menu(){
-	if(aberto){
-		fechar();
-		diminuir();
-	} else {
-		abrir();
-		expandir();
-	}
-}
-
-document.querySelector('.hamburguer').onclick= menu;
