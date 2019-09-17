1bdb82174ea21d20d61a0e895a7aa1ab7335a690
EKaynan <erickaynan11@gmail.com>
2019-09-17 13:59:16 -0400

    Adicionando funções as organelas da CA

---
+++ paginas/celula_animal/js/app.js
@@ -0,0 +1,462 @@
+const tx1= document.querySelector('.explicacao_1');
+const tx2= document.querySelector('.explicacao_2');
+const tx3= document.querySelector('.explicacao_3');
+const tx4= document.querySelector('.explicacao_4');
+const tx5= document.querySelector('.explicacao_5');
+const tx6= document.querySelector('.explicacao_6');
+const tx7= document.querySelector('.explicacao_7');
+
+var cont=1;
+
+//Textos
+function texto_mitocondria(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= 'I, I wish you could swim Like the dolphins Like dolphins can swim Though nothing, nothing will keep us together We can beat them, forever and ever Oh, we can be heroes just for one day'
+
+
+	tx2.innerHTML= 'I, I will be King And you, you will be Queen Though nothing will drive them away We can be heroes just for one day We can be us just for one day';
+	tx3.innerHTML= ' I, I can remember (I remember) Standing by the wall (By the wall) And the guns, shot above our heads (Over our heads) And we kissed, as though nothing could fall (Nothing could fall) And the shame, was on the other side';
+	tx4.innerHTML= ' Oh, we can beat them, forever and ever Then we could be heroes just for one day We can be heroes We can be heroes We can be heroes just for one day We can be heroes';
+}
+
+function texto_complex(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Ground Control to Major Tom Ground Control to Major Tom Take your protein pills and put your helmet on Ground Control to Major Tom Commencing countdown, engines on Check ignition and may God's love be with you Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two, One, Lift off";
+	tx2.innerHTML= "This is Ground Control to Major Tom You`ve really made the grade And the papers want to know whose shirts you wear Now it's time to leave the capsule if you dare";
+	tx3.innerHTML= "This is Major Tom to Ground Control I`m stepping through the door A`d I'm floating in a most peculiar way A`d the stars look very different today F`r here A` I sitting in a tin can F`r above the world P`anet Earth is blue A`d there's nothing I can do";
+}
+
+function texto_reticul(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Remember when you were young? You shone like the sun Shine on, you crazy diamond Now there's a look in your eyes Like black holes in the sky Shine on, you crazy diamond";
+	tx2.innerHTML= "You were caught in the crossfire Of childhood and stardom Blown on, the steel breeze Come on, you target for faraway laughter Come on, you stranger, you legend You martyr, and shine!";
+	tx3.innerHTML= "You reached for the secret too soon You cried for the moon Shine on you crazy diamond Threatened by shadows at night And exposed to the light Shine on you crazy diamond Well you wore out your welcome";
+	tx4.innerHTML= "With random precision Rode on the steel breeze Come on, you raver, you seer of visions Come on, you painter, you piper You prisoner, and shine!";
+}
+
+function texto_riboss(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Freezin' rests his head on a pillow made of concrete again ooh yeah Oh feelin' maybe he'll see a little betters any days ooh yeah";
+	tx2.innerHTML= "Oh hand out faces that he sees come again ain't that familiar Oh dark grin he can't help when he's happy he looks insane";
+	tx3.innerHTML= "Even flow Thoughts arrive like butterflies Oh he don't know, so he chases them away Someday yet he'll begin his life again Life again, life again";
+}
+
+function texto_lissos(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Who's in a bunker? Who's in a bunker? Women and children first And the children first And the children";
+	tx2.innerHTML= "I'll laugh until my head comes off I'll swallow till I burst Until I burst Until I";
+	tx3.innerHTML= "Who's in a bunker? Who's in a bunker? I have seen too much I haven't seen enough You haven't seen it";
+	tx4.innerHTML= "I'll laugh until my head comes off Women and children first And children first And children";
+	tx5.innerHTML= "Here I'm alive Everything all of the time Here I'm alive Everything all of the time";
+}
+
+function texto_peroxiss(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "We are scanning the scene in the city tonight We are looking for you to start up a fight There is an evil feeling in our brains But it is nothing new you know it drives us insane";
+	tx2.innerHTML= "Running, on our way hiding You will pay dying One thousand deaths Running, on our way hiding You will pay dying One thousand deaths";
+	tx3.innerHTML= "Searching, Seek and destroy Searching, Seek and destroy Searching, Seek and destroy Searching, Seek and destroy";
+}
+
+function texto_centri(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Lashing out the action, returning the reaction Weak are ripped and torn away Hypnotizing power, crushing all that cower Battery is here to stay";
+	tx2.innerHTML= "Smashing through the boundaries Lunacy has found me Cannot stop the battery Pounding out aggression Turns into obsession Cannot kill the battery Cannot kill the family Battery is found in me Battery Battery";
+}
+
+function texto_membr(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "There's a lady who's sure All that glitters is gold And she's buying a stairway to heaven";
+	tx2.innerHTML= "When she gets there she knows If the stores are all closed With a word she can get what she came for Oh oh oh oh and she's buying a stairway to heaven";
+	tx3.innerHTML= "There's a sign on the wall But she wants to be sure 'Cause you know sometimes words have two meanings";
+	tx4.innerHTML= "In a tree by the brook There's a songbird who sings Sometimes all of our thoughts are misgiving";
+	tx5.innerHTML= "Ooh, it makes me wonder Ooh, it makes me wonder";
+}
+
+function texto_cito(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "So, so you think you can tell Heaven from hell Blue skies from pain Can you tell a green field From a cold steel rail? A smile from a veil? Do you think you can tell?";
+	tx2.innerHTML= "Did they get you to trade Your heroes for ghosts? Hot ashes for trees? Hot air for a cool breeze? Cold comfort for change? Did you exchange A walk on part in the war For a lead role in a cage?";
+	tx3.innerHTML= "How I wish, how I wish you were here We're just two lost souls Swimming in a fish bowl Year after year Running over the same old ground And how we found The same old fears Wish you were here ";
+}
+
+function texto_celula(){
+	tx1.innerHTML= '';
+	tx2.innerHTML= '';
+	tx3.innerHTML= '';
+	tx4.innerHTML= '';
+	tx5.innerHTML= '';
+	tx6.innerHTML= '';
+	tx7.innerHTML= '';
+
+	tx1.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	tx2.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	tx3.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	tx4.innerHTML= "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+}
+
+function trocar_clique(valor){
+	// let classe=parseInt(valor)+1;
+	let el1=document.querySelector('.celula')
+	cont=(parseInt(valor)+1)
+	let el2='.l'+cont;
+	// alert(el2)
+	// alert(el1)
+	// alert(el2)
+	if(cont==1){
+		texto_celula();
+	}else if(cont==2){
+		texto_mitocondria();
+	} else if(cont==3){
+		texto_complex();
+	}else if(cont==4){
+		texto_reticul();
+	}else if(cont==5){
+		texto_riboss();
+	}else if(cont==6){
+		texto_lissos();
+	}else if(cont==7){
+		texto_peroxiss();
+	}else if(cont==8){
+		texto_centri();
+	}else if(cont==9){
+		texto_membr();
+	}else if(cont==10){
+		texto_cito();
+	}
+	anime({
+		targets: el1,
+		// position: 'absolute',
+		translateX: [
+			{value:'-100px',
+			duration:300},
+			{value:'5%'}
+		],
+		opacity:{
+			value:0,
+			duration:200
+		},
+		easing: 'linear',
+		display: 'none'
+	});
+	document.querySelector(el2).style.display='flex';
+	anime({
+		targets: el2,
+		// position: 'absolute',
+		translateX: '-5%',
+		opacity:1,
+		duration: 300,
+		easing: 'linear',
+		display: 'block'
+	});
+	document.querySelector('.avan').onclick= avancar_clique;
+	document.querySelector('.voltar').onclick= voltar_clique;
+}
+function avancar_clique(){
+	let el1='.l'+cont;
+	let el2='.l1';
+	anime({
+		targets: el1,
+		// position: 'absolute',
+		translateX: [
+			{value:'-100px',
+			duration:300},
+			{value:'5%'}
+		],
+		opacity:{
+			value:0,
+			duration:200
+		},
+		easing: 'linear',
+		display: 'none'
+	});
+	document.querySelector(el1).style.display='none';
+	document.querySelector(el2).style.display='flex';
+	anime({
+		targets: el2,
+		// position: 'absolute',
+		translateX: '-5%',
+		opacity:1,
+		duration: 300,
+		easing: 'linear',
+		display: 'block'
+	});
+	texto_celula();
+	document.querySelector('.avan').onclick= avancar_setas;
+	document.querySelector('.voltar').onclick= voltar_setas;
+	cont=1;
+}
+function voltar_clique(){
+	let el1='.l'+cont;
+	let el2='.l1';
+	texto_celula();
+	anime({
+		targets: el1,
+		// position: 'absolute',
+		translateX: [
+			{value:'10%'},
+			{value:'5%',
+			duration: 300
+			}
+		],
+		opacity:{
+			value:0,
+			duration:200
+		},
+		easing: 'linear'
+	});
+	document.querySelector(el1).style.display='none';
+	document.querySelector(el2).style.display='flex';
+	anime({
+		targets: el2,
+		// position: 'absolute',
+		translateX: [
+			{value:'-10%',
+			duration:10},
+			{value:'-5%',
+			duration: 300}
+		],
+		opacity:{
+			delay:10,
+			value:1,
+			duration: 200
+		},
+		easing: 'linear'
+	});
+	document.querySelector('.avan').onclick= avancar_setas;
+	document.querySelector('.voltar').onclick= voltar_setas;
+	cont=1;
+}
+function avancar_setas(){
+	let el1,el2;
+	if(cont==1){
+		el1='.l1';
+		el2='.l2';
+		texto_mitocondria();
+		cont++
+	}else if(cont==2){
+		el1='.l2';
+		el2='.l3'; 
+		texto_complex();
+		cont++
+	} else if(cont==3){
+		el1='.l3'
+		el2='.l4'
+		texto_reticul();
+		cont++
+	}else if(cont==4){
+		el1='.l4'
+		el2='.l5'
+		texto_riboss();
+		cont++
+	}else if(cont==5){
+		el1='.l5'
+		el2='.l6'
+		texto_lissos();
+		cont++
+	}else if(cont==6){
+		el1='.l6'
+		el2='.l7'
+		texto_peroxiss();
+		cont++
+	}else if(cont==7){
+		el1='.l7'
+		el2='.l8'
+		texto_centri();
+		cont++
+	}else if(cont==8){
+		el1='.l8'
+		el2='.l9'
+		texto_membr();
+		cont++
+	}else if(cont==9){
+		el1='.l9'
+		el2='.l10'
+		texto_cito();
+		cont++
+	}else if(cont==10){
+		el1='.l10'
+		el2='.l1'
+		texto_celula();
+		cont=1
+	}
+
+	anime({
+		targets: el1,
+		// position: 'absolute',
+		translateX: [
+			{value:'-100px',
+			duration:300},
+			{value:'5%'}
+		],
+		opacity:{
+			value:0,
+			duration:200
+		},
+		easing: 'linear',
+		display: 'none'
+	});
+	document.querySelector(el1).style.display='none';
+	document.querySelector(el2).style.display='flex';
+	anime({
+		targets: el2,
+		// position: 'absolute',
+		translateX: '-5%',
+		opacity:1,
+		duration: 300,
+		easing: 'linear',
+		display: 'block'
+	});
+
+}
+
+function voltar_setas(){
+	let el1,el2;
+	if(cont==1){
+		el1='.l1';
+		el2='.l10';
+		texto_cito();
+		cont=10
+	}else if(cont==10){
+		el1='.l10';
+		el2='.l9'; 
+		texto_membr();
+		cont--
+	} else if(cont==9){
+		el1='.l9'
+		el2='.l8'
+		texto_centri();
+		cont--
+	}else if(cont==8){
+		el1='.l8'
+		el2='.l7'
+		texto_peroxiss();
+		cont--
+	}else if(cont==7){
+		el1='.l7'
+		el2='.l6'
+		texto_lissos();
+		cont--
+	}else if(cont==6){
+		el1='.l6'
+		el2='.l5'
+		texto_riboss();
+		cont--
+	}else if(cont==5){
+		el1='.l5'
+		el2='.l4'
+		texto_reticul();
+		cont--
+	}else if(cont==4){
+		el1='.l4'
+		el2='.l3'
+		texto_complex();
+		cont--
+	}else if(cont==3){
+		el1='.l3'
+		el2='.l2'
+		texto_cito();
+		cont--
+	}else if(cont==2){
+		el1='.l2'
+		el2='.l1'
+		texto_celula();
+		cont--
+	}
+
+	anime({
+		targets: el1,
+		// position: 'absolute',
+		translateX: [
+			{value:'10%'},
+			{value:'5%',
+			duration: 300
+			}
+		],
+		opacity:{
+			value:0,
+			duration:200
+		},
+		easing: 'linear'
+	});
+	anime({
+		targets: el2,
+		// position: 'absolute',
+		translateX: [
+			{value:'-10%',
+			duration:10},
+			{value:'-5%',
+			duration: 300}
+		],
+		opacity:{
+			delay:10,
+			value:1,
+			duration: 200
+		},
+		easing: 'linear'
+	});
+	document.querySelector(el1).style.display='none';
+	document.querySelector(el2).style.display='flex';
+}
